c02540f51bff510ea99d3dd67ea9e4de5f881e29
bion0625 <stxtory@gmail.com>
2024-02-03 22:19:58 +0900

    package update and delete repository test

--- src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
+++ src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
@@ -58,4 +58,37 @@ class PackageRepositoryTest {
         assertEquals(packageEntity1.getPackageSeq(), packageEntities.get(0).getPackageSeq());
     }
 
+    @Test
+    public void test_updateCountAndPeriod() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디프로필 이벤트 4개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        // when
+        int updatedCount = packageRepository.updateCountAndPeriod(packageEntity.getPackageSeq(), 30, 120);
+        final PackageEntity updatedPackageEntity = packageRepository.findById(packageEntity.getPackageSeq()).get();
+
+        // then
+        assertEquals(1, updatedCount);
+        assertEquals(30, updatedPackageEntity.getCount());
+        assertEquals(120, updatedPackageEntity.getPeriod());
+    }
+
+    @Test
+    public void test_delete() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할 이용권");
+        packageEntity.setCount(1);
+        PackageEntity newPackageEntity = packageRepository.save(packageEntity);
+
+        // when
+        packageRepository.deleteById(newPackageEntity.getPackageSeq());
+
+        //then
+        assertTrue(packageRepository.findById(newPackageEntity.getPackageSeq()).isEmpty());
+    }
+
 }
