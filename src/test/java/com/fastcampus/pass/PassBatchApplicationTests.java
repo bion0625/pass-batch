8ff86d4e2b21338874541c8b190c9344c9783f45
bion0625 <stxtory@gmail.com>
2024-02-04 12:04:14 +0900

    PassBatchApplicationTests contextConfiguration setting

--- src/test/java/com/fastcampus/pass/PassBatchApplicationTests.java
+++ src/test/java/com/fastcampus/pass/PassBatchApplicationTests.java
@@ -1,9 +1,16 @@
 package com.fastcampus.pass;
 
+import com.fastcampus.pass.config.TestBatchConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = TestBatchConfig.class)
 class PassBatchApplicationTests {
 
 	@Test
